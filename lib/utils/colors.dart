ecf9d6935664cf19b65a8799acb7562f696bdf20
San Kyaw Htwe <sankyawhtwe.personal@gmail.com>
2025-02-10 14:19:55 +0630

    Implemented login screen ui

---
+++ lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(94, 37, 114, 1);
+const kAppBlack = Color.fromRGBO(0, 0, 0, 1);
+const kIconColorGrey = Color.fromRGBO(170, 170, 170, 1);
+const kHintTextColor = Color.fromRGBO(0, 0, 0, 0.5);
+const kAppWhite = Color.fromRGBO(255, 255, 255, 1);
+const kTextColor = Color.fromRGBO(37, 37, 37, 1);
+const kIconOutlineColor = Color.fromRGBO(41, 45, 50, 1);
+const kSentMessageColor = Color.fromRGBO(0, 173, 181, 1);
+const kReceiveMessageColor = Color.fromRGBO(242, 247, 251, 1);
+const kMessageTimeColor = Color.fromRGBO(121, 124, 123, 1);
+const kSurfaceGrey = Color.fromRGBO(217, 217, 217, 1);
+const kProfileIconsColor = Color.fromRGBO(95, 99, 104, 1);
+const kErrorColor = Color.fromRGBO(255, 0, 0, 1);
---
+++ lib/utils/string.dart
@@ -0,0 +1,8 @@
+const kWelcomeTitleText = 'Welcome back';
+const kWelcomeBodyText = 'sign in to access your account';
+const kEmailHintText = 'Email';
+const kPasswordHintText = 'Password';
+const kForgotPasswordText = 'Forgot password?';
+const kLoginButtonText = 'Log In';
+const kDontHaveAccountText = 'Don\'t have an account?';
+const kRegisterButtonText = 'Register';
