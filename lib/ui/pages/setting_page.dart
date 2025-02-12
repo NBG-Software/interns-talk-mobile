3a0d6ea0e2e6e765d18d93971f0d82e4519f4496
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2025-02-12 15:32:34 +0630

    Implemented Setting page ui

---
+++ lib/ui/pages/setting_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Text(
+          'Setting',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ),
+      body: _BodyView(),
+    );
+  }
+}
+
+class _BodyView extends StatefulWidget {
+  const _BodyView({super.key});
+
+  @override
+  State<_BodyView> createState() => _BodyViewState();
+}
+
+class _BodyViewState extends State<_BodyView> {
+  bool isActive = false;
+  String appVersion = "Loading...";
+
+  Future<void> loadAppVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      appVersion = packageInfo.version;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadAppVersion();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.settings_outlined),
+            title: Text('Version'),
+            trailing: Text(appVersion),
+          ),
+          ListTile(
+            leading: Icon(CupertinoIcons.lock),
+            title: Text('Change Password'),
+            trailing: Icon(CupertinoIcons.forward),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings_outlined),
+            title: Text('Terms and Conditions'),
+            trailing: Icon(CupertinoIcons.forward),
+          ),
+        ],
+      ),
+    );
+  }
+}
