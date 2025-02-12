0601d9ce5d237209a539377ac29b17af51d821d7
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2025-02-12 18:11:11 +0630

    Edited profile and edit profile page ui

--- lib/ui/pages/edit_profile_page.dart
+++ lib/ui/pages/edit_profile_page.dart
@@ -29,132 +29,176 @@ class _BodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final firstNameController = TextEditingController();
-    final lastNameController = TextEditingController();
-    return Padding(
-      padding: EdgeInsets.all(30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Edit Profile Here',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              SizedBox(height: 20,),
-              Text('Please enter the new profile detail')
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomTextFormField(
-                      controller: firstNameController,
-                      keyboardType: TextInputType.text,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return kFirstNameErrorText;
-                        }
-                        return null;
-                      },
-                      hintText: kFirstNameHint,
-                      hintTextColor: kHintTextColor,
-                      fillColor: kTextFieldContainer,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 16),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Expanded(
-                    child: CustomTextFormField(
-                      keyboardType: TextInputType.text,
-                      controller: lastNameController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return kLastNameErrorText;
-                        }
-                        return null;
-                      },
-                      hintText: kLastNameHint,
-                      hintTextColor: kHintTextColor,
-                      fillColor: kTextFieldContainer,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 16),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: kMarginMedium1x,
-              ),
-              CustomTextFormField(
-                suffixIconPath: kEmailIcon,
-                readOnly: true,
-                hintText: 'yourmail@gmail.com',
-                iconColor: kIconColorGrey,
-                fillColor: kTextFieldContainer,
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TitleContent(),
+                    SizedBox(height: 20,),
+                    ProfileForm(),
+                  ],
                 ),
-                suffixPadding: EdgeInsets.only(right: 12),
-              ),
-              SizedBox(
-                height: kMarginMedium1x,
-              ),
-              CustomTextFormField(
-                suffixIconPath: kLockIcon,
-                readOnly: true,
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                iconColor: kIconColorGrey,
-                suffixPadding: EdgeInsets.only(right: 12),
-                hintText: '•',
-                fillColor: kTextFieldContainer,
-                hintTextColor: kHintTextColor,
-              ),
-              SizedBox(
-                height: kMarginMedium1x,
-              ),
-              CustomTextFormField(
-                suffixIconPath: kLockIcon,
-                readOnly: true,
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                iconColor: kIconColorGrey,
-                suffixPadding: EdgeInsets.only(right: 12),
-                hintText: '•',
-                fillColor: kTextFieldContainer,
-                hintTextColor: kHintTextColor,
-              ),
-            ],
-          ),
-          Expanded(child: SizedBox()),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 42, vertical: 64),
-            child: FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                minimumSize: Size(double.infinity, 52),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8), // Rounded corners
-                ),
-              ),
-              onPressed: () {},
-              child: Text('Confirm'),
+                Spacer(),
+                ConfirmButton(),
+                SizedBox(height: 64)
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class TitleContent extends StatelessWidget {
+  const TitleContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Edit Profile Here',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        SizedBox(height: 20,),
+        Text('Please enter the new profile detail')
+      ],
+    );
+  }
+}
+
+class ProfileForm extends StatelessWidget {
+  const ProfileForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final firstNameController = TextEditingController();
+    final lastNameController = TextEditingController();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: CustomTextFormField(
+                controller: firstNameController,
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return kFirstNameErrorText;
+                  }
+                  return null;
+                },
+                hintText: kFirstNameHint,
+                hintTextColor: kHintTextColor,
+                fillColor: kTextFieldContainer,
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: CustomTextFormField(
+                keyboardType: TextInputType.text,
+                controller: lastNameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return kLastNameErrorText;
+                  }
+                  return null;
+                },
+                hintText: kLastNameHint,
+                hintTextColor: kHintTextColor,
+                fillColor: kTextFieldContainer,
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: kMarginMedium1x,
+        ),
+        CustomTextFormField(
+          suffixIconPath: kEmailIcon,
+          readOnly: true,
+          hintText: 'yourmail@gmail.com',
+          iconColor: kIconColorGrey,
+          fillColor: kTextFieldContainer,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
+          ),
+          suffixPadding: EdgeInsets.only(right: 12),
+        ),
+        SizedBox(
+          height: kMarginMedium1x,
+        ),
+        CustomTextFormField(
+          suffixIconPath: kLockIcon,
+          readOnly: true,
+          contentPadding:
+          EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          iconColor: kIconColorGrey,
+          suffixPadding: EdgeInsets.only(right: 12),
+          hintText: '•',
+          fillColor: kTextFieldContainer,
+          hintTextColor: kHintTextColor,
+        ),
+        SizedBox(
+          height: kMarginMedium1x,
+        ),
+        CustomTextFormField(
+          suffixIconPath: kLockIcon,
+          readOnly: true,
+          contentPadding:
+          EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          iconColor: kIconColorGrey,
+          suffixPadding: EdgeInsets.only(right: 12),
+          hintText: '•',
+          fillColor: kTextFieldContainer,
+          hintTextColor: kHintTextColor,
+        ),
+      ],
+    );
+  }
+}
+
+class ConfirmButton extends StatelessWidget {
+  const ConfirmButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 42,vertical: 64),
+      child: FilledButton(
+        style: FilledButton.styleFrom(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          minimumSize: Size(double.infinity, 52),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8), // Rounded corners
+          ),
+        ),
+        onPressed: () {},
+        child: Text('Confirm'),
+      ),
+    );
+  }
+}
+
+
--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:interns_talk_mobile/ui/pages/edit_profile_page.dart';
+import 'package:interns_talk_mobile/ui/pages/setting_page.dart';
 import 'package:interns_talk_mobile/utils/images.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -25,71 +27,119 @@ class _BodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-      child: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        width: screenWidth,
+        height: screenHeight,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
             children: [
-              Image.network(
-                'https://framerusercontent.com/images/XMW8cln5SZReUZI5zimRfTIBA.jpg',
-                scale: 1.0,
-                width: 148,
-              ),
+              ProfileDataView(),
               SizedBox(
-                height: 8,
+                height: 54,
               ),
-              InkWell(
-                child: Row(
-                  children: [
-                    Image.asset(kUploadIcon),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text('Upload'),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 52,
-              ),
-              ListTile(
-                leading: Icon(CupertinoIcons.person_circle),
-                title: Text('Edit Profile'),
-                trailing: Icon(CupertinoIcons.forward),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings_outlined),
-                title: Text('Setting'),
-                trailing: Icon(CupertinoIcons.forward),
-              ),
-              ListTile(
-                leading: Icon(CupertinoIcons.headphones),
-                title: Text('Help Center'),
-                trailing: Icon(CupertinoIcons.forward),
-              ),
+              ProfileMenuItems(),
+              Spacer(),
+              LogOutButton(),
+              SizedBox(height: 64)
             ],
           ),
-          Expanded(child: SizedBox()),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 42, vertical: 64),
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                minimumSize: Size(double.infinity, 52),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8), // Rounded corners
-                ),
-                side: BorderSide(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 2,
-                ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileDataView extends StatelessWidget {
+  const ProfileDataView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network(
+            'https://framerusercontent.com/images/XMW8cln5SZReUZI5zimRfTIBA.jpg',
+            scale: 1.0,
+            width: 148,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        InkWell(
+          child: Row(
+            children: [
+              Image.asset(kUploadIcon),
+              SizedBox(
+                width: 8,
               ),
-              onPressed: () {},
-              child: Text('Log Out'),
-            ),
-          )
-        ],
+              Text('Upload'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProfileMenuItems extends StatelessWidget {
+  const ProfileMenuItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const EditProfilePage()));
+          },
+          leading: Icon(CupertinoIcons.person_circle),
+          title: Text('Edit Profile'),
+          trailing: Icon(CupertinoIcons.forward),
+        ),
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const SettingPage()));
+          },
+          leading: Icon(Icons.settings_outlined),
+          title: Text('Setting'),
+          trailing: Icon(CupertinoIcons.forward),
+        ),
+      ],
+    );
+  }
+}
+
+class LogOutButton extends StatelessWidget {
+  const LogOutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 42, vertical: 64),
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          minimumSize: Size(double.infinity, 52),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8), // Rounded corners
+          ),
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 2,
+          ),
+        ),
+        onPressed: () {},
+        child: Text('Log Out'),
       ),
     );
   }
