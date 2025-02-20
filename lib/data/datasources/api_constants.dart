0e5c4d25c67b801efb14ea6e04d8ec4863d0a36d
San Kyaw Htwe <sankyawhtwe.personal@gmail.com>
2025-02-20 19:50:55 +0630

    Edited base url

--- lib/data/datasources/api_constants.dart
+++ lib/data/datasources/api_constants.dart
@@ -1,1 +1,3 @@
-const kBaseUrl = 'https://intern-talks.nbgsoftware.com/api/v1';
+// const kBaseUrl = 'https://intern-talks.nbgsoftware.com/api/v1';
+// const kBaseUrl = 'http://192.168.1.213:8000/api/v1';
+const kBaseUrl = 'http://47.129.56.224/api/v1';
