9de295b112eb2f10cf0261601e5f3a5c71de86e2
San Kyaw Htwe <sankyawhtwe.personal@gmail.com>
2025-02-25 10:03:02 +0630

    Edit text constants

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -1,3 +1,4 @@
+// Login Screen Texts
 const kWelcomeTitleText = 'Welcome back';
 const kWelcomeBodyText = 'sign in to access your account';
 const kEmailHintText = 'Email';
@@ -6,6 +7,8 @@ const kForgotPasswordText = 'Forgot password?';
 const kLoginButtonText = 'Log In';
 const kDontHaveAccountText = 'Don\'t have an account?';
 const kRegisterButtonText = 'Register';
+
+// Register Screen Texts
 const kRegisterTitle = 'Get Started';
 const kRegisterBodyText = 'by creating a free account';
 const kFirstNameHint = 'First Name';
@@ -14,9 +17,20 @@ const kValidEmailHint = 'Valid Email';
 const kStrongPasswordHint = 'Strong Password';
 const kConfirmPasswordHint = 'Confirm Password';
 const kHaveAccountText = 'Already have an account?';
-const kAuthTokenKey = 'authToken';
+
+// Error Messages
 const kFirstNameErrorText = "First name can't be empty";
 const kLastNameErrorText = "Last name can't be empty";
 const kEmailEmptyErrorText = 'Please enter a email';
-const kPasswordEmptyErrorText = 'Password can\'t be empty';
-const kPasswordNotMatchErrorText = "Password does not match";
+const kPasswordEmptyErrorText = 'Password cannot be empty';
+const kPasswordNotMatchErrorText = "Passwords don't match";
+const kPasswordShortErrorText = 'Password must be at least 8 characters';
+const kEmailInvalidErrorText = 'Please enter a valid email';
+const kPasswordUpperCaseErrorText =
+    'Password must contain at least one uppercase letter';
+const kPasswordLowerCaseErrorText =
+    'Password must contain at least one lowercase letter';
+const kPasswordDigitErrorText = 'Password must contain at least one digit';
+
+// Other Constants
+const kAuthTokenKey = 'authToken';
