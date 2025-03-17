2a9c80d41155272636ca5118eaebbe353b529a36
San Kyaw Htwe <sankyawhtwe.personal@gmail.com>
2025-03-17 09:36:35 +0630

    Add keys to registration page for testing

--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -121,7 +121,7 @@ class __BodyViewState extends State<_BodyView> {
                       children: [
                         Expanded(
                           child: CustomTextFormField(
-                            key: Key('firt_name_field'),
+                            key: Key('register_textField_firstName'),
                             controller: _firstNameController,
                             keyboardType: TextInputType.text,
                             autoValidateMode: AutovalidateMode.onUnfocus,
@@ -143,7 +143,7 @@ class __BodyViewState extends State<_BodyView> {
                         ),
                         Expanded(
                           child: CustomTextFormField(
-                            key: Key('last_name_field'),
+                            key: Key('register_textField_lastName'),
                             autoValidateMode: AutovalidateMode.onUnfocus,
                             keyboardType: TextInputType.text,
                             controller: _lastNameController,
@@ -166,7 +166,7 @@ class __BodyViewState extends State<_BodyView> {
                       height: kMarginMedium1x,
                     ),
                     CustomTextFormField(
-                      key: Key('email_field'),
+                      key: Key('register_textField_email'),
                       controller: _emailController,
                       suffixIcon: Image.asset(kEmailIcon),
                       keyboardType: TextInputType.emailAddress,
@@ -186,7 +186,7 @@ class __BodyViewState extends State<_BodyView> {
                       height: kMarginMedium1x,
                     ),
                     CustomTextFormField(
-                      key: Key('password_field'),
+                      key: Key('register_textField_password'),
                       controller: _strongPasswordController,
                       keyboardType: TextInputType.visiblePassword,
                       obscureText: !isVisible,
@@ -213,7 +213,7 @@ class __BodyViewState extends State<_BodyView> {
                       height: kMarginMedium1x,
                     ),
                     CustomTextFormField(
-                      key: Key('confirm_password_field'),
+                      key: Key('register_textField_confirmPassword'),
                       controller: _confirmPasswordController,
                       keyboardType: TextInputType.visiblePassword,
                       obscureText: isObscured,
@@ -248,7 +248,7 @@ class __BodyViewState extends State<_BodyView> {
                       height: kMarginMedium1x,
                     ),
                     FilledButton(
-                        key: Key('register_button'),
+                        key: Key('register_button_register'),
                         onPressed: () async {
                           if (_formKey.currentState!.validate()) {
                             await _summitForm();
@@ -277,6 +277,7 @@ class __BodyViewState extends State<_BodyView> {
                     width: 8,
                   ),
                   InkWell(
+                      key: ValueKey('register_button_goToLogin'),
                       onTap: () {
                         Navigator.of(context).pushAndRemoveUntil(
                             MaterialPageRoute(
