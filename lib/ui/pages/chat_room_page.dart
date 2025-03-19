77a40e636dbef923ba83b4c4bbc17ffe3a72646b
San Kyaw Htwe <sankyawhtwe.personal@gmail.com>
2025-03-19 15:28:58 +0630

    Revert "Add keys to chat room page for testing"
    This reverts commit aac48574b6730fab08bc6033337f9ca978155904.

--- lib/ui/pages/chat_room_page.dart
+++ lib/ui/pages/chat_room_page.dart
@@ -192,7 +192,6 @@ class _ChatRoomBodyViewState extends State<ChatRoomBodyView> {
           return Padding(
             padding: const EdgeInsets.only(top: 20),
             child: InkWell(
-              key: ValueKey('chatroom_button_chat'),
               onTap: () {
                 _startChatting(
                     chatId: chat.chatId ?? 0,
@@ -314,7 +313,6 @@ class _ChatRoomBodyViewState extends State<ChatRoomBodyView> {
                         ),
                       ),
                       IconButton(
-                          key: ValueKey('chatroom_button_createChat'),
                           onPressed: () {
                             _onChatCreate(mentorId: mentor.id ?? 0);
                           },
